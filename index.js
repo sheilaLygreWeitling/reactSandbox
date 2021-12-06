e9a266de186950cf5edd432a9e574a08e0fe87bf
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-12-06 13:53:20 +0100

    hej

---
+++ index.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+        name: "Özlem",
+        email: "hej@hej.hej"
+    },
+    {
+        name: "Trollos",
+        email: "TrollTrollos@hej.hej"
+    },
+    {
+        name: "Sheila",
+        email: "hej@hej.hej"
+    }
+]
