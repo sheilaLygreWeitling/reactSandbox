52ac1c6400f0be41ad58b3a2c851254d3f606d84
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-12-07 08:26:25 +0100

    add changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const user = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+}
+
 const users = [
     {
         name: "Özlem",
@@ -12,3 +18,67 @@ const users = [
         email: "hej@hej.hej"
     }
 ]
+
+
+//spread operator - Merger user og users
+const mergedUser = { ...user, ...users };
+console.log(mergedUser);
+
+//array method .map 
+const usernames = users.map(user => user.name);
+console.log(usernames);
+
+// Template Literals 
+const greeting = `hola ${usernames}`
+console.log(greeting);
+
+//loop igennem usernames 
+usernames.forEach(username => {
+    console.log(`Hola los ${username}! estàs bien?`);
+})
+
+//greetings with ternaries - does the same thing as a if-else statement 
+
+let ages = [
+    {
+        age: 26
+    },
+    {
+        age: 16
+    }
+]
+
+const newGreeting = ages.age > 18 ? "Hello, fellow adult" : "Hey kiddo";
+newGreeting;
+
+console.log(newGreeting);
+
+const memberAges = [
+    {
+        person: 16
+    },
+    {
+        person: 17
+    },
+    {
+        person: 18
+    },
+    {
+        person: 19
+    }
+];
+
+/* for (let index = 0; index < memberAges.length; index++) {
+
+    memberAges.person > 18 ? "Hello, fellow adult" : "Hey kiddo";
+}
+ */
+memberAges.forEach(memberAge => {
+    memberAge.person > 18 ? "Hello, fellow adult" : "Hey kiddo";
+    console.log(memberAge.person);
+});
+
+
+
+
+
